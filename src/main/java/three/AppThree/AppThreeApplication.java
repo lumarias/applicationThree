0e4b5e59247a427d3b94d09963fef0eac2ac8bf5
lariasto <luisam_arias@bancopopular.com.co>
2022-03-28 16:22:29 -0500

    Primer commit

---
+++ src/main/java/three/AppThree/AppThreeApplication.java
@@ -0,0 +1,13 @@
+package three.AppThree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppThreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/three/AppThree/AppThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package three.AppThree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
